62fc43ee711835ef8149bd684ac19a21c3ae7725
Khaled Helmy <khaledhelmy444@gmail.com>
2024-11-19 13:49:39 +0200

    Update 3_Index_Genome.sh

--- 3_Index_Genome.sh
+++ 3_Index_Genome.sh
@@ -4,8 +4,8 @@
 # Define paths to genome output directory, genome FASTA file, and annotation GTF file
 path=$(pwd) 
 genomeDir="${path}/Genome_Data"        # Output directory for the genome index
-genomeFastaFile="${path}/Genome_Data/Sesamum_indicum.fasta"     # Input genome FASTA file
-sjdbGTFfile="${path}/Genome_Data/Sesamum_indicum.gtf"          # Annotation GTF file
+genomeFastaFile="${path}/Genome_Data/reference_genome.fasta"     # Input genome FASTA file
+sjdbGTFfile="${path}/Genome_Data/annotation.gtf"          # Annotation GTF file
 
 
 # Run STAR genomeGenerate command
@@ -14,7 +14,6 @@ STAR --runThreadN 8 \
      --genomeDir "$genomeDir" \
      --genomeFastaFiles "$genomeFastaFile" \
      --sjdbGTFfile "$sjdbGTFfile" \
-     --genomeSAindexNbases 13 \
      --sjdbOverhang 99
 
 echo "STAR genome index generation complete."
